d500d6d82706434972b03299f67588420c9b3749
Raúl Castaño Canales <raul.castano.870@estudiante.iesoretania.es>
2020-11-28 03:21:25 +0100

    Implementación Get y Set en Libro

--- src/Entity/Libro.php
+++ src/Entity/Libro.php
@@ -60,4 +60,140 @@ class Libro
      * @var string
      */
     private $sinopsis;
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitulo(): string
+    {
+        return $this->titulo;
+    }
+
+    /**
+     * @param string $titulo
+     * @return Libro
+     */
+    public function setTitulo(string $titulo): Libro
+    {
+        $this->titulo = $titulo;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAutor(): string
+    {
+        return $this->autor;
+    }
+
+    /**
+     * @param string $autor
+     * @return Libro
+     */
+    public function setAutor(string $autor): Libro
+    {
+        $this->autor = $autor;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getEditorial(): string
+    {
+        return $this->editorial;
+    }
+
+    /**
+     * @param string $editorial
+     * @return Libro
+     */
+    public function setEditorial(string $editorial): Libro
+    {
+        $this->editorial = $editorial;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getAnioPublicacion(): int
+    {
+        return $this->anioPublicacion;
+    }
+
+    /**
+     * @param int $anioPublicacion
+     * @return Libro
+     */
+    public function setAnioPublicacion(int $anioPublicacion): Libro
+    {
+        $this->anioPublicacion = $anioPublicacion;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getIsbn(): int
+    {
+        return $this->isbn;
+    }
+
+    /**
+     * @param int $isbn
+     * @return Libro
+     */
+    public function setIsbn(int $isbn): Libro
+    {
+        $this->isbn = $isbn;
+        return $this;
+    }
+
+    /**
+     * @return int
+     */
+    public function getNumeroPaginas(): int
+    {
+        return $this->numeroPaginas;
+    }
+
+    /**
+     * @param int $numeroPaginas
+     * @return Libro
+     */
+    public function setNumeroPaginas(int $numeroPaginas = null): Libro
+    {
+        $this->numeroPaginas = $numeroPaginas;
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSinopsis(): string
+    {
+        return $this->sinopsis;
+    }
+
+    /**
+     * @param string $sinopsis
+     * @return Libro
+     */
+    public function setSinopsis(string $sinopsis): Libro
+    {
+        $this->sinopsis = $sinopsis;
+        return $this;
+    }
+
+
 }
